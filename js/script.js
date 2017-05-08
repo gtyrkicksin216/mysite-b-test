3a168129858609fd130e226a2417940011bd82b7
Ryan Wood <r0178658@gmail.com>
2017-05-08 16:30:16 -0400

    Added menu for page navigation and tested

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-console.log('Custom JS connected');
+// ONLY HERE TO CHECK FOR CONNECTIVITY IN CASE OF ISSUES
+//console.log('Custom JS connected');
 
+// CAUSES THE MENU & BUTTON TO DROP DOWN WHEN CLICKED AND RETURN TO NORMAL STATE WHEN CLICKED AGAIN
+$('#menu-button').on('click', function(){
+    $('.menu-inner').toggleClass('show');
+    $('.menu-top').toggleClass('button-show');
+    $('.button-inner').toggleClass('glyphicon-menu-down glyphicon-menu-up');
+});
+
+// CLOSES MENU IF A LINK IS CLICKED
+$('.menu-inner a').on('click', function(){
+    $('.menu-inner').toggleClass('show');
+    $('.menu-top').toggleClass('button-show');
+    $('.button-inner').toggleClass('glyphicon-menu-down glyphicon-menu-up');
+});
